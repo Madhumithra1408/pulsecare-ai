30200f661ab1d00fa77293cf678f7665f7e74a9f
Madhumithra1408 <25csea11madhumithra@gmail.com>
2026-06-27 11:32:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function showPage(pageId) {
+
+  let pages = document.querySelectorAll(".page");
+
+  pages.forEach((page) => {
+    page.style.display = "none";
+  });
+
+  document.getElementById(pageId).style.display = "block";
+
+}
+
+showPage("profile");
